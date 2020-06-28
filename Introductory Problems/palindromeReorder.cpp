bf14d02d4639add5cd3a7bf21fd919beb5486064
Denis <ivankovden99@gmail.com>
2020-06-28 13:58:04 +0300

    Add solution

--- Introductory Problems/palindromeReorder.cpp
+++ Introductory Problems/palindromeReorder.cpp
@@ -1,4 +1,53 @@
-//
-// Created by chappybunny on 27.06.2020.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    string input;
+    cin >> input;
+    map<char, int> mp;
+    for (char & i : input) {
+        if (!mp.find(i)->first) {
+            mp.insert(make_pair(i, 1));
+        } else
+            mp[i]++;
+    }
+    int cnt = 0;
+    for (auto& j : mp) {
+        if (j.second % 2 == 1)
+            cnt++;
+    }
+    if (cnt > 1) {
+        cout << "NO SOLUTION\n";
+    } else {
+        string out;
+        for (auto &j : mp) {
+            if (j.second % 2 != 1) {
+                for (int i = 0; i < j.second / 2; ++i) {
+                    out += j.first;
+                }
+            }
+        }
+        for (auto &j: mp) {
+            if (j.second % 2 == 1)
+                for (int i = 0; i < j.second; ++i) {
+                    out += j.first;
+                }
+        }
+        for (auto j = mp.end(); j != mp.begin(); --j) {
+            if (j->second % 2 != 1) {
+                for (int i = 0; i < j->second / 2; ++i) {
+                    out += j->first;
+                }
+            }
+        }
+        if (mp.begin()->second % 2 != 1) {
+            for (int k = 0; k < mp.begin()->second / 2; ++k) {
+                out += mp.begin()->first;
+            }
+        }
+        cout << out << "\n";
+    }
+    
+    return 0;
+}
