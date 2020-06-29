a26ae036bf922e6a2c71cf427eb18f7118cff8fe
Denis <ivankovden99@gmail.com>
2020-06-29 13:40:58 +0300

    Add solution

--- Introductory Problems/creatingStringsI.cpp
+++ Introductory Problems/creatingStringsI.cpp
@@ -1,4 +1,27 @@
-//
-// Created by chappybunny on 28.06.2020.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    string input;
+    cin >> input;
+
+    sort(input.begin(), input.end());
+    vector<char> permutation;
+    set<vector<char>> all;
+    for (auto &i : input) {
+        permutation.push_back(i);
+    }
+    do {
+        all.insert(permutation);
+    } while (next_permutation(permutation.begin(), permutation.end()));
+
+    cout << all.size() << "\n";
+    for (auto &i : all) {
+        for (auto &j : i) {
+            cout << j;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
