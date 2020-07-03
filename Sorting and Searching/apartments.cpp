9381158a6b68b4cde2cfe8ab4570546a4375d9c8
Denis <ivankovden99@gmail.com>
2020-07-03 17:53:27 +0300

    Add solution

--- Sorting and Searching/apartments.cpp
+++ Sorting and Searching/apartments.cpp
@@ -1,4 +1,38 @@
-//
-// Created by chappybunny on 03.07.2020.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll n, m, k, cnt = 0;
+    cin >> n >> m >> k;
+    vector<ll> a(n);
+    vector<ll> b(m);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
+    }
+    
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    int iter_a = 0;
+    int iter_b = 0;
+
+    while (iter_a < n && iter_b < m) {
+        if (abs(b[iter_b] - a[iter_a]) <= k) {
+            iter_b++;
+            iter_a++;
+            cnt++;
+        } else if (b[iter_b] > a[iter_a]) {
+            iter_a++;
+        } else {
+            iter_b++;
+        }
+    }
+
+    cout << cnt << "\n";
+    return 0;
+}
