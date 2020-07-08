38d9b611a081ea714cb126da5f580e8daa5c872b
Denis <ivankovden99@gmail.com>
2020-07-08 14:04:56 +0300

    Add solution

--- Sorting and Searching/stickLengths.cpp
+++ Sorting and Searching/stickLengths.cpp
@@ -1,4 +1,23 @@
-//
-// Created by chappybunny on 08.07.2020.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> p(n);
+    ll cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> p[i];
+    }
+
+    sort(p.begin(), p.end());
+    for (auto & i : p) {
+        cnt += abs(i - p[n / 2]);
+    }
+    cout << cnt << "\n";
+
+    return 0;
+}
