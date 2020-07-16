939c57deef7a0ed8a6b4a4703e432589675c4376
Denis <ivankovden99@gmail.com>
2020-07-16 17:50:29 +0300

    Add solution

--- Dynamic Programming/coinCombinationsI .cpp
+++ Dynamic Programming/coinCombinationsI .cpp
@@ -1,4 +1,37 @@
-//
-// Created by chappybunny on 15.07.2020.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+typedef long long ll;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+    vector<ll> c(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> c[i];
+    }
+    sort(c.begin(), c.end());
+    vector<ll> dp(x + 1);
+    dp[0] = 1;
+    for (int i = 1; i <= x; ++i) {
+        for (auto & j : c) {
+            if (i - j >= 0) {
+                dp[i] += dp[i - j];
+                dp[i] %= MOD;
+            }
+            else {
+                break;
+            }
+        }
+    }
+    cout << dp[x] << "\n";
+
+    return 0;
+}
